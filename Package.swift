bea04c040f91b576f9c2bc5f717a633fb5c1d8d2
Sergey Starukhin <star.s@me.com>
2020-01-06 14:09:04 +0100

    rename POSInputStreamLibrary to POSInputStreamDataSource

--- Package.swift
+++ Package.swift
@@ -8,11 +8,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "POSInputStreamLibrary",
-            targets: ["POSInputStreamLibrary"]),
-	     .library(
-	        name: "POSInputStreamCoreLibrary",
-	        targets: ["POSInputStreamCoreLibrary"]),
+           name: "POSInputStreamCoreLibrary",
+           targets: ["POSInputStreamCoreLibrary"]),
+        .library(
+            name: "POSInputStreamDataSource",
+            targets: ["POSInputStreamDataSource"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,13 +22,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "POSInputStreamLibrary",
+            name: "POSInputStreamCoreLibrary",
+            dependencies: []),
+        .target(
+            name: "POSInputStreamDataSource",
             dependencies: ["POSInputStreamCoreLibrary"]),
-	    .target(
-	        name: "POSInputStreamCoreLibrary",
-	        dependencies: []),
         .testTarget(
             name: "POSInputStreamLibraryTests",
-            dependencies: ["POSInputStreamLibrary"]),
+            dependencies: ["POSInputStreamDataSource"]),
     ]
 )
